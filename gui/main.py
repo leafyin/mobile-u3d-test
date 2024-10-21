f59fb2468bb974b3390272167fde4820aa07a2e2
leafyin <chinayinye@icloud.com>
2024-10-21 19:14:38 +0800

    更新远古内容

--- gui/main.py
+++ gui/main.py
@@ -1,77 +1,42 @@
 # encoding=utf-8
 
+import json
 import threading
 import unittest
 import platform
 import subprocess
 
-from example.ExampleTest import ExampleTest
-from pywebio.output import *
-from functools import partial
+from bottle import *
 
 from airtest.core.api import *
 
 
-class MainGUI:
-
-    def __init__(self):
-        # adb devices
-        self.devices = []
-        system = platform.system()
-        if system == 'Windows':
-            cmd = "..\\init.bat"
-        elif system in ['Darwin', 'Linux']:
-            cmd = "sh ../init.sh"
-        else:
-            return
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, encoding='utf-8')
-        stdout = process.stdout.readlines()
+@route('/')
+def dropdown():
+    response.content_type = 'application/json'
+    devices = []
+    system = platform.system()
+    if system == 'Windows':
+        cmd = "adb devices"
+    elif system in ['Darwin', 'Linux']:
+        cmd = "adb devices"
+    else:
+        return
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, encoding='utf-8')
+    # 处理子进程结束
+    process.wait()
+    # 确保文件关闭
+    with process.stdout as stdout:
+        stdout_str = stdout.readlines()
         # 去掉头部尾部
-        try:
-            stdout.pop(0)
-            stdout.pop(len(stdout) - 1)
-        except IndexError:
-            print("device not found")
-            return
-        for i in range(len(stdout)):
-            if ":" or "emulator" in stdout[i]:
-                self.devices.append(stdout[i].split("device")[0].strip("\t"))
-        print(f"{len(self.devices)} device have been found {self.devices}")
-
-        def buttons(d):
-
-            def onoff():
-                connect_device(f"Android:///{d}?cap_method=javacap&touch_method=adb")
-                keyevent("26")
-
-            def _exec_():
-                subprocess.Popen(f"python ..\\example\\ExampleTest.py {d}",
-                                 stdout=subprocess.PIPE,
-                                 shell=True,
-                                 encoding='utf-8')
-                toast(f"设备{d}执行用例")
-
-            return put_buttons(["开关", "运行"], onclick=[onoff, _exec_])
-
-        def table():
-            li = []
-            loader = unittest.TestLoader()
-            suite = loader.loadTestsFromTestCase(ExampleTest)
-            for case in suite:
-                k = [case]
-                li.append(k)
-            return put_table(li)
-
-        def init_gui():
-            # gui part
-            ggui = []
-            for d in self.devices:
-                ts = [d, table(), buttons(d)]
-                ggui.append(ts)
-            return put_table(ggui, header=["设备", "用例", "操作"])
-
-        put_button("运行所有已连接设备", onclick=lambda: toast("Clicked"))
-        init_gui()
+        stdout_str.pop(0)
+        stdout_str.pop(len(stdout_str) - 1)
+        for i in range(len(stdout_str)):
+            if ":" or "emulator" in stdout_str[i]:
+                devices.append(stdout_str[i].split("device")[0].strip("\t"))
+        print(f"{len(devices)} devices have been found {devices}")
+    return json.dumps(devices)
 
 
-gui = MainGUI()
+if __name__ == '__main__':
+    run(host='localhost', port=9547, debug=True, reloader=True)
